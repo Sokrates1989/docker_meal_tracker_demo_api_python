bf8348b02fd50608054305f7dbc508356c28d626
Patrick Michiels <it@patrickmichiels.de>
2024-10-24 12:37:43 +0200

    Added code watermark

--- src/models/credentialsItem.py
+++ src/models/credentialsItem.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2024 Patrick Michiels
+# All rights reserved.
+# This source code is licensed under the Evaluation License Agreement and
+# may not be used, modified, or distributed without explicit permission from the author.
+# This code is provided for evaluation purposes only.
+
 class CredentialsItem:
     """
     Model used to wrap user credentials sent to the API.
